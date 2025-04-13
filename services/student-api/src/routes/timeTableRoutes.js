ff4e7571c11b62c899aba5d24acd9d79f8f2d8a5
Mnat <HemnathJanardhanan>
2025-04-14 02:38:57 +0530

    Added Timetable Route

--- services/student-api/src/routes/timeTableRoutes.js
+++ services/student-api/src/routes/timeTableRoutes.js
@@ -1,8 +1,17 @@
-// import express from "express";
-// const router = express.Router();
-//
-// router.get('/daily',);
-// router.get('/weekly',);
-// router.get('/monthly',);
-//
-// module.exports = router
+import express from "express";
+import authMiddleWare from "../middleWares/authMiddleWare.js";
+import {getFixedTimetable} from "../controllers/timeTableController.js";
+const router = express.Router();
+
+/**
+ * Route: GET /api/student/fixed_timetable
+ * Functionality: Fetches the fixed timetable for a student based on their registration number.
+ * Input:
+ *   Header: { authorization: "Bearer token" }
+ * Output: Body: { timetable: [{ day: String, slots: [{ period_no: Number, course_name: String, faculty_name: String, room_no: String }] }] }
+ */
+router.get('/fixed-timetable', authMiddleWare, getFixedTimetable);
+
+
+
+export default router;
