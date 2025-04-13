5a78b3118271e71b7588a057f533a9ba48382b4c
simonacatherine <simonacatherine1991@gmail.com>
2025-04-13 21:07:20 +0530

    added timetable and feedback page

--- client/src/layouts/student/StudentLayout.jsx
+++ client/src/layouts/student/StudentLayout.jsx
@@ -1,15 +1,21 @@
+import React, { useState } from 'react';
+import StudentSidebar from '../../components/student/StudentSidebar';
 import { Outlet } from 'react-router-dom';
-import StudentSidebar from '../../components/student/StudentSidebar.jsx';
 
-const StudentLayout = () => {
+export default function StudentLayout() {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
     return (
-        <div className='flex'>
-            <StudentSidebar />
-            <div className='flex-1 p-10'>
+        <div className="flex">
+            <StudentSidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+
+            <main
+                className={`flex-1 mt-14 transition-all duration-300 ${
+                    isCollapsed ? 'ml-20' : 'ml-64'
+                }`}
+            >
                 <Outlet />
-            </div>
+            </main>
         </div>
     );
-};
-
-export default StudentLayout;
+}
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -9,7 +9,7 @@ const RoleCard = ({img,title,onClick}) => {
         <div className="flex items-center justify-center">
             <div
                 onClick={onClick}
-                className="flex flex-col items-center justify-center w-36 h-36 sm:w-52 sm:h-52 p-4 rounded-2xl shadow-lg hover:shadow-2xl border-4 border-blue/75 transition-transform duration-200 hover:scale-105 cursor-pointer overflow-hidden"
+                className="flex flex-col items-center justify-center w-36 h-36 sm:w-52 sm:h-52 p-4 rounded-2xl shadow-lg hover:shadow-2xl border-4 border-blue-400 transition-transform duration-200 hover:scale-105 cursor-pointer overflow-hidden"
             >
                 <img src={img} alt={title} className="w-full h-full object-cover" />
                 <p className="font-semibold text-center mt-2 text-gray-700">{title}</p>
