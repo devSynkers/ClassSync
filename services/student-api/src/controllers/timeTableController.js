df71f5fbc73edf1cf64ff7a97400a160fca5f01d
celshee <sheenaxelin@gmail.com>
2025-04-14 17:24:28 +0530

    merged mnat works with sheena

--- services/student-api/src/controllers/timeTableController.js
+++ services/student-api/src/controllers/timeTableController.js
@@ -5,9 +5,7 @@ export const getFixedTimetable = async (req, res) => {
 
     try {
         // Get batch_id from student
-        const studentResult = await db.query(
-            'SELECT batch_id FROM student.students WHERE reg_no = $1',
-            [regNo]
+        const studentResult = await db.query('SELECT batch_id FROM student.students WHERE reg_no = $1', [regNo]
         );
 
         if (studentResult.rows.length === 0) {
@@ -17,26 +15,25 @@ export const getFixedTimetable = async (req, res) => {
         const batchId = studentResult.rows[0].batch_id;
 
         // Get timetable slots for this batch
-        const timetableQuery = `
-            SELECT ts.day_of_week, ts.period_no, 
-                   c.name AS course_name, 
-                   f.name AS faculty_name,
-                   ts.room_id
-            FROM admin.timetable_slots ts
-            JOIN admin.courses c ON ts.course_id = c.id
-            JOIN faculty.faculties f ON ts.faculty_id = f.id
-            WHERE ts.batch_id = $1
-            ORDER BY 
-                CASE ts.day_of_week
-                    WHEN 'Monday' THEN 1
-                    WHEN 'Tuesday' THEN 2
-                    WHEN 'Wednesday' THEN 3
-                    WHEN 'Thursday' THEN 4
-                    WHEN 'Friday' THEN 5
-                    ELSE 6
-                END,
-                ts.period_no;
-        `;
+        const timetableQuery =
+            "SELECT ts.day_of_week, ts.period_no, " +
+            "c.name AS course_name, " +
+            "f.name AS faculty_name, " +
+            "ts.room_id " +
+            "FROM admin.timetable_slots ts " +
+            "JOIN admin.courses c ON ts.course_id = c.id " +
+            "JOIN faculty.faculties f ON ts.faculty_id = f.id " +
+            "WHERE ts.batch_id = $1 " +
+            "ORDER BY " +
+            "CASE ts.day_of_week " +
+            "WHEN 'Monday' THEN 1 " +
+            "WHEN 'Tuesday' THEN 2 " +
+            "WHEN 'Wednesday' THEN 3 " +
+            "WHEN 'Thursday' THEN 4 " +
+            "WHEN 'Friday' THEN 5 " +
+            "ELSE 6 " +
+            "END, " +
+            "ts.period_no;";
 
         const timetableResult = await db.query(timetableQuery, [batchId]);
         console.log(timetableResult)
