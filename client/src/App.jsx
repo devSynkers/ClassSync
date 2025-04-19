b9cd88e8be34b66d7429d677b07c7a9d595ceffd
Raasika2510 <raasikamageshwaran2510@gmail.com>
2025-04-19 21:10:53 +0530

    Merge conflict solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import StudentSidebar from "./components/student/StudentSidebar.jsx";
 import StudentLogin from './pages/student/StudentLogin.jsx'
 import Landing from './pages/Landing.jsx'
-import StudentLayout from "./layouts/student/StudentLayout.jsx";
+import StudentLayout from "./layouts/students/StudentLayout.jsx";
 import StudentDashboard from "./pages/student/StudentDashboard.jsx";
 import StudentClassTimetable from "./pages/student/StudentClassTimetable.jsx";
 import FacultyLogin from "./pages/faculty/FacultyLogin.jsx";
@@ -24,7 +24,7 @@ function App() {
               <Route path="/faculty/dashboard" element={<FacultyDashBoard/>}/>
 
 
-            <Route path="/student" element={<StudentLayout />}>
+            <Route path="/student" element={<StudentSidebar />}>
                 <Route path="dashboard" element={<StudentDashboard />} />
                 <Route path="calendar" element={<StudentDashboard />} />
                 <Route path="classtimetable" element={<StudentClassTimetable />} />
